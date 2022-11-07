7699befd5affdf0ef914f956a1e71594e291d8b8
leocassiosilva <leocassiosilva1234@gmail.com>
2022-11-07 09:39:04 -0300

    alter models, add novos atributos

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Empresa(models.Model):
     nome = models.CharField(max_length=100, help_text='Nome da empresa')
+
+
+    def __str__(self):
+        return self.nome
--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,8 +1,13 @@
 from django.db import models
-
-
+from django.contrib.auth.models import User
+from apps.empresas.models import Empresa
+from apps.departamentos.models import Departamento
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamento = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
+
 
     def __str__(self):
         return self.nome
