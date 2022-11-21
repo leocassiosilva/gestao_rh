31036048d0a93e46d3c832bc16f966e407982465
leocassiosilva <leocassiosilva1234@gmail.com>
2022-11-21 11:40:14 -0300

    funcionarios

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
+from .views import FuncionariosList
 
 urlpatterns = [
-    # path('funcionarios/', include('apps.funcionarios.urls')),
+    path('', FuncionariosList.as_view(), name='list_funcionario'),
 ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from .models import Funcionario
+class FuncionariosList(ListView):
+    model = Funcionario
 
-# Create your views here.
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        queryset = Funcionario.objects.filter(empresa=empresa_logada)
+        return queryset
